95295d3ba55ba11dba0694eb87effb390dd1865b
Tyler Hunt <tyhunt04@gmail.com>
2019-06-02 15:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue) {
   if (someValue < 400) {
     result = 'This one is on me!'
   }
-  elsif (someValue > 2000) {
+  else if (someValue > 2000) {
     result = 'I will gladly take your thirty bucks.'
   }
   return result
